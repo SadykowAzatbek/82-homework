67e6eacf2c6cc71347af9dd137729b6fa5328527
azatbek <sadykowazatbek@gmail.com>
2024-02-22 13:03:23 +0600

    Добавлен компонент Artists.tsx, добавлен artistsSlice, который получает массив исполнителей через thunk getArtists. А компонент Artists выводит как элемент на страницу полученные данные.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,21 @@
 import './App.css';
+import {Grid, Typography} from '@mui/material';
+import {Route, Routes} from 'react-router-dom';
+import Artists from './components/Artists/Artists.tsx';
 
 function App() {
 
   return (
     <>
-
+      <header>
+        <Grid>
+          <Typography variant="h3">Music App</Typography>
+        </Grid>
+      </header>
+      <Routes>
+        <Route path="/" element={<Artists />} />
+        <Route path="*" element={<strong>Not found!</strong>} />
+      </Routes>
     </>
   )
 }
---
+++ frontend/src/components/Artists/Artists.tsx
@@ -0,0 +1,53 @@
+import {Card, CardActionArea, CardContent, CardMedia, CircularProgress, Grid, styled, Typography} from '@mui/material';
+import {useEffect} from 'react';
+import {useAppDispatch, useAppSelector} from '../../App/hooks.ts';
+import {selectArtists, selectIsLoading} from '../store/artistsSlice.ts';
+import {getArtists} from '../store/asynсThunks.ts';
+import {Link as RouterLink} from 'react-router-dom';
+
+const Artists = () => {
+  const dispatch = useAppDispatch();
+  const artists = useAppSelector(selectArtists);
+  const isLoading = useAppSelector(selectIsLoading);
+
+  useEffect(() => {
+    const fetchUrl = async () => {
+      await dispatch(getArtists());
+    };
+
+    void fetchUrl();
+  }, [dispatch]);
+
+  const ImageCardMedia = styled(CardMedia)({
+    height: 0,
+    paddingTop: '56.25%',
+  })
+
+  return (
+    <>
+      <Grid container spacing={3} mt={3}>
+        {!isLoading ? artists.map((elem) => (
+          <Grid item xs={3} key={elem._id}>
+            <RouterLink to={`/artist/album`}>
+              <Card sx={{ maxWidth: 345 }}>
+                <CardActionArea>
+                  {elem.image !== null ? <ImageCardMedia image={'http://localhost:8000' + '/' + elem.image}/> : ''}
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      {elem.name}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      {elem.info}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </RouterLink>
+          </Grid>
+        )) : <CircularProgress />}
+      </Grid>
+    </>
+  );
+};
+
+export default Artists;
